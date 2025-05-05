ef2fb2ae43b09d54378e47e3b2d4a7d8631884e5
CesarMCuellarCha <cesarmcuellar@gmail.com>
2025-05-05 12:20:51 -0500

    Primeros ejercicios javascript

---
+++ js/app.js
@@ -0,0 +1,14 @@
+//let numero1 = parseInt(prompt("Ingrese un Número: "))
+//let numero2 = parseInt(prompt("Ingrese un Número: "))
+
+let numero1 =5
+let numero2 = 2
+let suma = numero1+numero2
+let resta=numero1-numero2
+let multiplicacion=numero1*numero2
+let division=numero1/numero2
+console.log(suma)
+//document.writeln("Suma: " + suma + "<br>")
+///document.writeln(`Resta: ${resta}` + "<br>")
+//document.writeln("Multiplicación: " + multiplicacion + "<br>")
+//document.writeln("División: " + division + "<br>")
